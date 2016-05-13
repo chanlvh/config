9af71730b962f649ec01d8182420f32ea645e258
Chan Le <chanvn@gmail.com>
2016-05-13 13:20:10 +0700

    colorful comment print out

--- setup.sh
+++ setup.sh
@@ -3,26 +3,31 @@
 #echo "Print out each command before execute"
 #set -x
 
-echo "******> COLORED BASH"
+# print out comment line with color
+p() {
+  echo "$(tput setaf 2)******> $1$(tput sgr0)"
+}
+
+p "COLORED BASH"
 sed -i -e 's/#force_color_prompt=yes/force_color_prompt=yes/g' ~/.bashrc
 source ~/.bashrc # reload bashrc
 
-echo "******> GENERATE KEY"
+p "GENERATE KEY"
 cat /dev/zero | ssh-keygen -q -t rsa -C "server@chan.io" -N ""
 
-echo "******> INSTALL BUILD-ESSENTIAL, VIM AND DOKKU PREREQUISITES"
+p "INSTALL BUILD-ESSENTIAL, VIM AND DOKKU PREREQUISITES"
 sudo apt-get update -qq 
 sudo apt-get --yes --force-yes -qq install build-essential vim apt-transport-https
 
-echo "******> INSTALL DOCKER"
+p "INSTALL DOCKER"
 wget -nv -O - https://get.docker.com/ | sh
 
-echo "******> ADD DOKKU REPO"
+p "ADD DOKKU REPO"
 wget -nv -O - https://packagecloud.io/gpg.key | apt-key add -
 echo "deb https://packagecloud.io/dokku/dokku/ubuntu/ trusty main" | sudo tee /etc/apt/sources.list.d/dokku.list
 sudo apt-get update -qq
 
-echo "******> CONFIG DOKKU "
+p "CONFIG DOKKU "
 HOST_NAME="note.cool"
 KEY=$(curl -sS https://raw.githubusercontent.com/chanlvh/config/master/id_rsa.pub)
 echo "dokku dokku/web_config boolean false" | sudo debconf-set-selections
@@ -31,19 +36,20 @@ echo "dokku dokku/hostname string $HOST_NAME" | sudo debconf-set-selections
 echo "dokku dokku/skip_key_file boolean true" | sudo debconf-set-selections
 echo "dokku dokku/key_file string /root/.ssh/id_rsa.pub" | sudo debconf-set-selections
 
-echo "******> INSTALL DOKKU"
+p "INSTALL DOKKU"
 sudo apt-get --yes --force-yes -qq install dokku
 sudo dokku plugin:install-dependencies --core
 
-echo "******> INSTALL MONGO PLUGIN"
+p "INSTALL MONGO PLUGIN"
 dokku plugin:install https://github.com/dokku/dokku-mongo.git mongo
 
-echo "******> ADD MY PUBLIC KEY"
-sshcommand acl-add dokku $KEY
+p "ADD MY PUBLIC KEY"
+sshcommand acl-add dokku "$KEY"
 
-echo "******> MAKE DOKKU RECOGNIZE GITHUB KEYS"
+p "MAKE DOKKU RECOGNIZE GITHUB KEYS"
+# run su - dokku separately cause weird bugs when things appear after bash script end
 su - dokku -c 'ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts'
 
-echo "******> DEPLOYMENT KEY"
+p "DEPLOYMENT KEY"
 su - dokku -c 'cat /dev/zero | ssh-keygen -q -t rsa -C "dokku@chan.io" -N ""'
 su - dokku -c 'cat ~/.ssh/id_rsa.pub'
